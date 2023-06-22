ae91986facd2db94547ff259af680cc6560bd3f8
micolay14 <130623351+micolay14@users.noreply.github.com>
2023-06-21 20:14:21 -0500

    Add files via upload

---
+++ punteros.h
@@ -0,0 +1,11 @@
+#ifndef PUNTEROS_H
+#define PUNTEROS_H
+
+void sumar(int* num1, int* num2, int* resultado);
+void restar(int* num1, int* num2, int* resultado);
+void multiplicar(int* num1, int* num2, int* resultado);
+void dividir(int* num1, int* num2, float* resultado);
+void sumaArregloEscalar(float* arreglo, int t, float escalar);
+void compararCadena(int tc1, int tc2, char* cadena1, char* cadena2);
+
+#endif
